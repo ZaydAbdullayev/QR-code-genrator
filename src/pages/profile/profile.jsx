a6480b309d1587a4c2190c71a5bd047c71e69b5e
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2023-09-14 19:00:03 +0500

    new section

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -2,25 +2,9 @@ import React from "react";
 import "./profile.css";
 import { useSwipeable } from "react-swipeable";
 
-const config = {
-  delta: 10, // min distance(px) before a swipe starts
-  preventDefaultTouchmoveEvent: false, // preventDefault on touchmove, *See Details*
-  trackTouch: true, // track touch input
-  trackMouse: false, // track mouse input
-  rotationAngle: 0, // set a rotation angle
-};
-
-export const Profile = () => {
-  const handlers = useSwipeable({
-    onSwiped: (eventData) => console.log("User Swiped!", eventData),
-    ...config,
-  });
-
+export const Nothificate = () => {
   return (
     <div className="profile">
-      <div {...handlers} style={{ touchAction: "pan-y" }}>
-        Swipe here
-      </div>
     </div>
   );
 };
--- src/router.js
+++ src/router.js
@@ -1,13 +1,11 @@
 import "./assets/global.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/home";
-import { Profile } from "./pages/profile/profile";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
